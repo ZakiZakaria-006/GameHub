de121ab2ffe6409330d9566501968939c8b62899
ZakiZakaria-006 <yomandiguna0306@gmail.com>
2025-08-07 21:58:04 +0700

    Update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,10 +25,6 @@ export default function RootLayout({
           crossOrigin="anonymous"
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap"
-          rel="stylesheet"
-        />
-        <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
           integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
